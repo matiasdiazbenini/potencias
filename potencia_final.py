f8a1ee1986669659b2d7f53f47f78b4ce01e4071
mati <matiasdiazbenini@gmail.com>
2022-01-11 01:30:14 -0300

    Finalizada la potenciacion, agregadas las otras 5 operaciones. Falta terminarlas

--- potencia_final.py
+++ potencia_final.py
@@ -23,34 +23,102 @@
 # LIMITE = LIMITE DE RESULTADO
 # POTENCIADOR =  NUMERO INICIAL
 
-preguntapot = """
-Qué número querés potenciar?:
-"""
+def suma():
+    pass
 
-preguntalim = """
-Cuál es el resultado máximo que te gustaría obtener?:
-"""
+def resta():
+    pass
 
-preguntacom = """
-Desde qué potencia te gustaría comenzar? (se admiten negativos):
-"""
+def multiplicacion():
+    pass
 
-opcionpot = int(input(preguntapot))
-opcionlim = int(input(preguntalim))
-opcioncom = int(input(preguntacom))
+def division():
+    pass
+
+def potenciacion():
+
+    def primera():
+        preguntapot = """
+        ¿Qué número querés potenciar? (recomendado entre el 1 y el 10):
+        """
+
+        preguntalim = """
+        ¿Cuál es el resultado máximo que te gustaría obtener? (el tope de carga, se recomienda número alto):
+        """
+
+        preguntacom = """
+        ¿Desde qué potencia te gustaría comenzar? (se admiten negativos):
+        """
+
+        opcionpot = int(input(preguntapot))
+        opcionlim = int(input(preguntalim))
+        opcioncom = int(input(preguntacom))
+
+        LIMITE = opcionlim
+        x = opcionpot
+
+        potenciador = opcioncom
+        potencia_x = x**potenciador
+        while potencia_x < LIMITE:
+            print(str(x) + " elevado a  " + str(potenciador) + " es igual a: " + str(potencia_x))
+            potenciador = potenciador + 1
+            potencia_x = x**potenciador
+
+
+    def segunda():
+        numero = "numero a potenciar: "
+        x = int(input(numero))
+        potencia = "potencia a la cual elevarlo: "
+        y =int(input(potencia))
+
+        print(str(x**y))
+    
+    decision = """
+    Como te gustaria usar el programa de potencias? Inserte solamente un numero como respuesta.
+
+    Opcion 1: - Calcular una serie de potencias de un numero especifico dentro de un rango determinado
+    Opcion 2: - Calcular rapido una potencia tal de un numero dado
+    """
+    eleccion = input(decision)
+
+    if eleccion == "1":
+        primera()
+    elif eleccion == "2":
+        segunda()
+    else:
+        print("Respuesta errónea, escriba nuevamente")
+
+def racionalizacion():
+    pass
 
 def run():
-    LIMITE = opcionlim
-    x = opcionpot
+    programa = """
+    Que programa le gustaria usar hoy?
 
-    potenciador = opcioncom
-    potencia_x = x**potenciador
-    while potencia_x < LIMITE:
-        print(str(x) + " elevado a  " + str(potenciador) + " es igual a: " + str(potencia_x))
-        potenciador = potenciador + 1
-        potencia_x = x**potenciador
+    Opcion 1: - Suma
+    Opcion 2: - Resta
+    Opcion 3: - Multiplicacion
+    Opcion 4: - Division
+    Opcion 5: - Potenciacion
+    Opcion 6: - Racionalizacion
+    """
+    choose = input(programa)
+
+    if choose == "1":
+        suma()
+    if choose == "2":    
+        resta()
+    if choose == "3":
+        multiplicacion()
+    if choose == "4":
+        division()
+    if choose == "5":
+        potenciacion()
+    if choose == "6":
+        racionalizacion()
 
 if __name__ == "__main__":
     run()
+
 
 
